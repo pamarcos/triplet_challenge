278a743d58f509c3223f4cde8ab62ff7c414402d
Pablo Marcos Oltra <pablo.marcos.oltra@gmail.com>
2019-01-10 17:09:37 +0100

    Update travis.yml to use triplet_challenge_stats

---
+++ after_success.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt install time cloc valgrind
+
+wget https://github.com/sharkdp/hyperfine/releases/download/v1.5.0/hyperfine_1.5.0_amd64.deb && sudo dpkg -i hyperfine_1.5.0_amd64.deb
+wget https://github.com/pamarcos/triplet_challenge_stats/releases/download/continuous/triplet_challenge_stats.tar.gz && tar xvzf triplet_challenge_stats.tar.gz
+
+lscpu
+cloc $SRC_DIR
+
+# 3 runs to warm up
+for i in `seq 1 3`; do 
+    $TRIPLET_CHALLENGE
+done
+
+# Benchmark speed
+./triplet_challenge_stats clear
+hyperfine -r 30 "LD_PRELOAD=./triplet_challenge_preload.so $TRIPLET_CHALLENGE"
+./triplet_challenge_stats read
+
+# Benchmark memory using GNU Time (Maximum resident set size)
+$(which time) -v $TRIPLET_CHALLENGE
+
+# Benchmark memory using Valgrind
+valgrind --tool=massif --time-unit=B --massif-out-file=valgrind.out $TRIPLET_CHALLENGE
+ms_print valgrind.out
